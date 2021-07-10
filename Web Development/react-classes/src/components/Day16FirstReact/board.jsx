3d03fd4941f4de4e77dce8375242e70e71a1adf4
Aaliya7516 <aaliya7516@gmail.com>
2021-07-10 16:24:29 +0530

    THA 16 & 18a

--- Web Development/react-classes/src/components/Day16FirstReact/board.jsx
+++ Web Development/react-classes/src/components/Day16FirstReact/board.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
+import style from './style.module.css'
 
-const border = {
-    border: '20px solid',
-    padding: '1px',
-    display: 'grid',
-    margin: 'auto',
-    width:'fit-content',
-    gridTemplateRows: 'repeat(8, auto)',
-    gridTemplateColumns: 'repeat(8, auto)'
-}
-const filled ={
-    height: '20px',
-    width: '20px',
-    backgroundColor: 'black'
-}
-const unfilled ={
-    height: '20px',
-    width: '20px',
-    backgroundColor: 'white'
-}
-var arr = [0,1,2,3,4,5,6,7]
 const Board =() =>{
+    const gridSet = [];
+    for (let i = 0; i<64; i++){
+        gridSet.push( <div key={i} className={style.boardGrid}></div>)
+    }
     return(
-        <div style={border}>
-            {arr.map((i) => (
-                arr.map((j) => (
-                    <div key={i*10+j} style={((i+j) % 2===0)? filled:unfilled}></div>
-                ))
-            ))}
+        <div className={style.boardBorder}>
+            {gridSet}
         </div>
     );
 };
--- Web Development/react-classes/src/components/Day18IfElseInReact/board.jsx
+++ Web Development/react-classes/src/components/Day18IfElseInReact/board.jsx
@@ -1,31 +1,12 @@
 import React from 'react';
-
-const border = {
-    border: '20px solid',
-    padding: '1px',
-    display: 'grid',
-    margin: 'auto',
-    width:'fit-content',
-    gridTemplateRows: 'repeat(8, auto)',
-    gridTemplateColumns: 'repeat(8, auto)'
-}
-const filled ={
-    height: '20px',
-    width: '20px',
-    backgroundColor: 'black'
-}
-const unfilled ={
-    height: '20px',
-    width: '20px',
-    backgroundColor: 'white'
-}
+import style from './style.module.css'
 var arr = [0,1,2,3,4,5,6,7]
 const Board =() =>{
     return(
-        <div style={border}>
+        <div className={style.boardBorder}>
             {arr.map((i) => (
                 arr.map((j) => (
-                    <div key={i*10+j} style={((i+j) % 2===0)? filled:unfilled}></div>
+                    <div key={i*10+j} className={((i+j) % 2===0)? style.boardFilled: style.boardUnfilled}></div>
                 ))
             ))}
         </div>
