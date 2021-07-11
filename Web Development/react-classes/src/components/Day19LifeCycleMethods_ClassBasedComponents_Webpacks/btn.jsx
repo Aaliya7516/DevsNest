214058c77ed204fabea6ea0a90c5ec3fe364a09e
Aaliya7516 <aaliya7516@gmail.com>
2021-07-11 17:25:59 +0530

    Updated THA 19

--- Web Development/react-classes/src/components/Day19LifeCycleMethods_ClassBasedComponents_Webpacks/btn.jsx
+++ Web Development/react-classes/src/components/Day19LifeCycleMethods_ClassBasedComponents_Webpacks/btn.jsx
@@ -1,7 +1,37 @@
+import React from 'react';
+
+class Button extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleCount =() =>{
+        this.setState((prev) => {
+            return {
+                count: prev.count + 1
+            }
+        } )
+    }
+
+    render(){
+        return (
+            <button onClick={this.handleCount}> {this.state.count} </button>
+        )
+    }
+}
+
 const BtnSet = () =>{
-    console.log("data");
     return (
-        <p>There are 4 counter component instances that each manage their own state.</p> 
+        <div>
+            <p>There are 4 counter component instances that each manage their own state.</p> 
+            <Button />   
+            <Button />   
+            <Button />   
+            <Button />   
+        </div> 
     )
 }
 export default BtnSet;
