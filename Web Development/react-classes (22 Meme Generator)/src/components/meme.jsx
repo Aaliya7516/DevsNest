79da93d719de6f12b7fe2786aef8b1f99d651c98
Aaliya7516 <aaliya7516@gmail.com>
2021-07-28 21:10:23 -0700

    Added THA 25

--- Web Development/react-classes (22 Meme Generator)/src/components/meme.jsx
+++ Web Development/react-classes (22 Meme Generator)/src/components/meme.jsx
@@ -1,34 +1,57 @@
 import { useState } from "react";
 
-const Meme = (({meme, setMeme}) => {
-    const [form,setForm] = useState({
+const Meme = (({ meme, setMeme }) => {
+    const [form, setForm] = useState({
         template_id: meme.id,
         username: "the_stranger",
         password: "meme123!",
         boxes: [],
 
     })
-    const generateMeme = () =>{
+    const [imgGenerated, setImgGenerated] = useState(false)
+    const generateMeme = () => {
         let url = `https://api.imgflip.com/caption_image?template_id=${form.template_id}&username=${form.username}&password=${form.password}`
         form.boxes.map((box, index) => {
             url += `&boxes[${index}][text]=${box.text}`;
         });
         fetch(url)
-        .then((res) => res.json())
-        .then((data) => { setMeme({...meme, url: data.data.url}) }  )
+            .then((res) => res.json())
+            .then((data) => { setMeme({ ...meme, url: data.data.url })
+        })
+        setImgGenerated(true)
+    }
+    const downloadFile = () => {
+        fetch(meme.url, {
+            method: "GET",
+            headers: {}
+        })
+            .then(response => {
+                response.arrayBuffer().then(function (buffer) {
+                    const url = window.URL.createObjectURL(new Blob([buffer]));
+                    const link = document.createElement("a");
+                    link.href = url;
+                    link.setAttribute("download", "meme.png");
+                    document.body.appendChild(link);
+                    link.click();
+                });
+            })
+            .catch(err => {
+                window.alert(err);
+                console.log(err);
+            });
     }
     return (
         <div className=" text-center mt-7">
-            <div>
+            <div className="w-full content-center">
                 <img
-                    className="h-96 w-auto"
+                    className="h-96 m-auto my-3 w-auto"
                     src={meme.url} alt="" />
             </div>
             <div className="inputs">
-                {[...Array(meme.box_count)].map((el, index) =>(
-                    <input 
-                        key={index} 
-                        type="text" 
+                {[...Array(meme.box_count)].map((el, index) => (
+                    <input
+                        key={index}
+                        type="text"
                         className="input
                         p-3 
                         m-3
@@ -36,19 +59,19 @@ const Meme = (({meme, setMeme}) => {
                         border-transparent 
                         focus:outline-none
                         focus:border-purple-600 
-                        rounded" 
-                        placeholder={`Add Caption ${index+1}`} 
-                        onChange={(e) =>{
+                        rounded"
+                        placeholder={`Add Caption ${index + 1}`}
+                        onChange={(e) => {
                             const newBoxes = form.boxes;
-                            newBoxes[index] = { text: e.target.value};
+                            newBoxes[index] = { text: e.target.value };
                             setForm({ ...form, boxes: newBoxes });
                         }}
                     />
                 ))}
             </div>
             <div>
-                { form.boxes.length === meme.box_count ?
-                <button className="bg-white
+                {form.boxes.length === meme.box_count ?
+                    <button className="bg-white
                  hover:bg-purple-600 
                  hover:bg-opacity-75 
                  text-purple-600 
@@ -62,7 +85,7 @@ const Meme = (({meme, setMeme}) => {
                  rounded 
                  p-3 
                  m-3" onClick={generateMeme}>Generate Meme</button>
-                 : null
+                    : null
                 }
                 <button className="bg-white
                  hover:bg-purple-600 
@@ -77,7 +100,24 @@ const Meme = (({meme, setMeme}) => {
                  hover:border-transparent 
                  rounded 
                  p-3 
-                 m-3" onClick={(()=> setMeme(null))}>Choose template</button>
+                 m-3" onClick={(() => setMeme(null) && setImgGenerated(false))}>Choose template</button>
+                {(form.boxes.length === meme.box_count) && (imgGenerated === true) ?
+                    <button className="bg-white
+                hover:bg-purple-600 
+                hover:bg-opacity-75 
+                text-purple-600 
+                font-semibold 
+                hover:text-white 
+                py-2 
+                px-4 
+                border 
+                border-purple-600 
+                hover:border-transparent 
+                rounded 
+                p-3 
+                m-3" onClick={downloadFile} >Download Meme Image</button>
+                    : null
+                }
             </div>
         </div>
     );
