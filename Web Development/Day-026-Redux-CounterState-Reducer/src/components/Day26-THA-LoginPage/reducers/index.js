79562cae5bbfdb7adf494f7d3148f43e78113f40
Aaliya7516 <aaliya7516@gmail.com>
2021-08-05 19:11:32 -0700

    .

--- Web Development/Day-026-Redux-CounterState-Reducer/src/components/Day26-THA-LoginPage/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import inputReducer from "./inputReducer";
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({ inputReducer });
-
-export default rootReducer;
--- Web Development/Day-026-Redux-CounterState-Reducer/src/components/Day26-class-reducer/reducers/counterReducer.js
+++
@@ -1,7 +0,0 @@
-const counterReducer = (state = 0, action) => {
-    if (action.type === "INC_NUMBER") { state += 1 }
-    else if (action.type === "DEC_NUMBER") { state -= 1 }
-    return state;
-}
-
-export default counterReducer;
