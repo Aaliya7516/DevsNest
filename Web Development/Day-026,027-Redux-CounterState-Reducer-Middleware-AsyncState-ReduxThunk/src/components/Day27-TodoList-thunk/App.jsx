a32087a55aa74706fa1f52d2eadff8dd2cfd2188
Aaliya7516 <aaliya7516@gmail.com>
2021-08-07 17:48:42 -0700

    THA 28,29

--- Web Development/Day-026,027-Redux-CounterState-Reducer-Middleware-AsyncState-ReduxThunk/src/components/Day27-TodoList-thunk/App.jsx
+++ Web Development/Day-026,027-Redux-CounterState-Reducer-Middleware-AsyncState-ReduxThunk/src/components/Day27-TodoList-thunk/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import AddTodo from './addTodo';
 import ListTodo from './listTodo';
+import store from './store'
 
 const App = () =>{
     return (
@@ -12,4 +14,11 @@ const App = () =>{
     )
 }
 
-export default App;
+const Base =() => {
+    return (
+        <Provider store = {store}>
+            <App />
+        </Provider>
+    )
+}
+export default Base;
